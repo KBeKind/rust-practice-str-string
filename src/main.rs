3ae58661baa023953d46fde7ca884ce9bf72f7fd
KBeKind <kyle@kbekind.com>
2024-01-01 11:49:03 -0600

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::num;
+use std::vec;
 
 
 
@@ -48,6 +49,15 @@ fn modifiable() {
     println!("slice: {:?}", slice);
 }
 
+fn get_item(index: usize){
+    //let index: usize = 3;
+    let a_vec: Vec<i32> = vec![1, 2, 3, 4, 5];
+    // UNWRAPPING THE OPTION CREATED WITH .get()
+    let value:&i32 = a_vec.get(index).unwrap();
+    println!("the value at index {} is: {:?}", index, value);
+
+}
+
 
 fn main() {
 
@@ -93,5 +103,32 @@ fn main() {
     modifiable();
 
 
+    let a_vec = vec![1, 2, 3, 4, 5];
+
+    // RETRIEVE A VALUE AT A SPECIFIED INDEX
+
+    let third_value = a_vec[2];
+    println!("{}", third_value);
+
+    // RETRIEVE THE LAST VALUE IN A VECTOR
+    // TWO DIFFERENT WAYS TO ACCESS THE LAST VALUE IN A VECTOR
+    let last_value: i32 = a_vec[a_vec.len() - 1];
+    // UNWRAP IS USED TO UNWRAP THE Some() FOR THE Option
+    // IF IT IS EMPTY THE unwrap() WILL CAUSE A PANIC
+    let last_value2: &i32 = a_vec.last().unwrap();
+    // THIS IS A SAFE WAY TO ACCESS THE LAST VALUE IN A VECTOR VIA USING Option IN CASE VECTOR IS EMPTY
+    // WITHOUT THE UNWRAP IT SHOWS THE Some()
+    let last_value3_option: Option<&i32> = a_vec.last();
+    println!("last_value1: {}", last_value);
+    println!("last_value2: {}", last_value2);
+    println!("last_value3: {:?}", last_value3_option);
+
+
+    match a_vec.first() {
+        Some(first_value) => println!("first value: {}", first_value),
+        None => println!("no first value"),
+    }
+
+    get_item(3);
 
 }
